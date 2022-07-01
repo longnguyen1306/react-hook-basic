7f5040bafef4f63634e129cc6299a2a25afeb2f9
LongNguyen <ldc.longnd@gmail.com>
2022-07-01 13:50:19 +0700

    add sass

---
+++ src/views/Nav/index.js
@@ -0,0 +1,14 @@
+import '../../styles/Nav/index.scss';
+
+const Index = () => {
+    return (
+        <div className="topnav">
+            <a className="active" href="/">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    )
+}
+
+export default Index;
